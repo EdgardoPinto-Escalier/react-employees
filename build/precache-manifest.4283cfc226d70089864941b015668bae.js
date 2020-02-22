1c807241313f443d53151967b61eee3746e051d9
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-02-22 20:51:00 +0100

    added new directory

---
+++ build/precache-manifest.4283cfc226d70089864941b015668bae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697221dc1d052cb6999d9c3c34a8f518",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c1c230ed29da3f86e9b",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0266c37689d9185c914b",
+    "url": "/static/css/main.22016121.chunk.css"
+  },
+  {
+    "revision": "2c1c230ed29da3f86e9b",
+    "url": "/static/js/2.a5406642.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a5406642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0266c37689d9185c914b",
+    "url": "/static/js/main.9efd0195.chunk.js"
+  },
+  {
+    "revision": "938051c8aa3846bd8b2f",
+    "url": "/static/js/runtime-main.29311f15.js"
+  }
+]);
